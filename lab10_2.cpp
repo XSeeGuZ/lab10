2f9eb35134bef8b4ed1d84e5bfbc32076893be3c
TelTRiNoZ <title34718@gmail.com>
2022-12-27 15:51:05 +0700

    lab10_2 complete

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
 using namespace std;
 
+void printO(int N,int M){
+	if (N<=0 or M<=0)
+	{
+		cout << "Invalid input";
+	}
+	else
+	{
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < M; j++)
+			{
+				cout << 'O';
+			}
+		cout << '\n';
+		}
+		
+	}
+	
+}
+
 //Write the function printO() here
 
 int main(){
